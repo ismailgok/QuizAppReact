4e26848e40b9fa33d2141706a0b40b0802dffae1
İsmail Gök <ismayilgok16@gmail.com>
2022-05-10 09:29:18 +0300

    Create Question.js

---
+++ src/Components/Question.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Question = ({ questions, currentQuestion, onClickButton }) => {
+    return (
+        <div className="question">
+            <span>Soru:{currentQuestion + 1}</span>{" "}
+            <h2>{questions[currentQuestion].title}</h2>
+            {questions[currentQuestion].answers.map((item, index) => (
+                <button key={index} onClick={() => onClickButton(item.correct)}>
+                    {item.answer}
+                </button>
+            ))}
+        </div>
+    );
+};
+
+export default Question;
