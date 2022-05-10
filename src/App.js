31da33d8fe37097f8a53998c587e5c820f4b1dca
İsmail Gök <ismayilgok16@gmail.com>
2022-05-10 09:27:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,86 +1,190 @@
 import { useState } from "react";
 import "./App.css";
-import Quiz from "./Components/Quiz";
+import Question from "./Components/Question";
+
+const questions = [
+    {
+        id: 1,
+        title: "Atatürk kaç yılında doğdu?",
+        answers: [
+            {
+                answer: "1936",
+                correct: false,
+            },
+            {
+                answer: "1938",
+                correct: false,
+            },
+            {
+                answer: "1881",
+                correct: true,
+            },
+            {
+                answer: "1900",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "İstanbulu kim fethetti?",
+        answers: [
+            {
+                answer: "Fatih Sultan Mehmet",
+                correct: true,
+            },
+            {
+                answer: "Kanuni Sultan Süleyman",
+                correct: false,
+            },
+            {
+                answer: "Yavuz Sultan Selim",
+                correct: false,
+            },
+            {
+                answer: "Mustafa Kemal Atatürk",
+                correct: false,
+            },
+            {
+                answer: "Yıldırım Bayezit",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "Bu sene kim şampiyon?",
+        answers: [
+            {
+                answer: "Fenerbahçe",
+                correct: false,
+            },
+            {
+                answer: "Trabzonspor",
+                correct: true,
+            },
+            {
+                answer: "Beşiktaş",
+                correct: false,
+            },
+            {
+                answer: "Galatasaray",
+                correct: false,
+            },
+            {
+                answer: "Başakşehir",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "1. Dünya savaşı ne zaman başladı?",
+        answers: [
+            {
+                answer: "1940",
+                correct: false,
+            },
+            {
+                answer: "1918",
+                correct: false,
+            },
+            {
+                answer: "1914",
+                correct: true,
+            },
+            {
+                answer: "1920",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "2. Dünya savaşı ne zaman başladı?",
+        answers: [
+            {
+                answer: "1939",
+                correct: true,
+            },
+            {
+                answer: "1950",
+                correct: false,
+            },
+            {
+                answer: "1951",
+                correct: false,
+            },
+            {
+                answer: "1940",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 6,
+        title: "2022 yılında Rusya kime saldırdı?",
+        answers: [
+            {
+                answer: "Polonya",
+                correct: false,
+            },
+            {
+                answer: "Ukrayna",
+                correct: true,
+            },
+            {
+                answer: "Türkiye",
+                correct: false,
+            },
+            {
+                answer: "ABD",
+                correct: false,
+            },
+        ],
+    },
+    {
+        id: 7,
+        title: "18 yaşından büyük müsün?",
+        answers: [
+            {
+                answer: "Evet",
+                correct: true,
+            },
+            {
+                answer: "Hayır",
+                correct: false,
+            },
+        ],
+    },
+];
 
 function App() {
-    const quiz = [
-        {
-            id: 1,
-            question: "Atatürk kaç yılında doğdu",
-            a: "1938",
-            b: "1880",
-            c: "1881",
-            d: "1900",
-            corretAnswer: "1881",
-        },
-        {
-            id: 2,
-            question: "İstanbulu kim fethetti",
-            a: "Fatih Sultan Mehmet",
-            b: "Kanuni Sultan Süleyman",
-            c: "Yavuz Sultan Selim",
-            d: "Mustafa Kemal Atatürk",
-            corretAnswer: "Fatih Sultan Mehmet",
-        },
-        {
-            id: 3,
-            question: "Bu sene kim şampiyon?",
-            a: "Fenerbahçe",
-            b: "Trabzonspor",
-            c: "Galatasaray",
-            d: "Beşiktaş",
-            corretAnswer: "Trabzonspor",
-        },
-        {
-            id: 4,
-            question: "1. Dünya savaşı ne zaman başladı",
-            a: "1940",
-            b: "1914",
-            c: "1905",
-            d: "1900",
-            corretAnswer: "1914",
-        },
-        {
-            id: 5,
-            question: "2. Dünya savaşı ne zaman başladı",
-            a: "1939",
-            b: "1920",
-            c: "1950",
-            d: "1945",
-            corretAnswer: "1939",
-        },
-        {
-            id: 6,
-            question: "2022 yılında Rusya kime saldırdı?",
-            a: "Polonya",
-            b: "Ukrayna",
-            c: "ABD",
-            d: "Çin",
-            corretAnswer: "Ukrayna",
-        },
-    ];
-
-    const [counter, setCounter] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showQuestion, setShowQuestion] = useState(true);
     const [score, setScore] = useState(0);
-    const [completed, setCompleted] = useState(false);
 
-    const onClickButton = (e) => {
-        e.preventDefault();
-
-        if (quiz[counter].id < quiz.length) {
-            setCounter(counter + 1);
-        } else {
-            alert("Quiz Bitti");
-            setCompleted(true);
-        }
-
-        if (e.target.value === quiz[counter].corretAnswer) {
+    const onClickButton = (correct) => {
+        const nextQuestion = currentQuestion + 1;
+        if (correct === true) {
             setScore(score + 1);
+        }
+        if (nextQuestion < questions.length) {
+            console.log(nextQuestion, questions.length);
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowQuestion(false);
         }
     };
 
     return (
         <div className="App">
+<<<<<<< HEAD
+            {showQuestion ? (
+                <Question
+                    questions={questions}
+                    currentQuestion={currentQuestion}
+=======
             {completed ? (
                 `
                Doğru cevap sayısı ${score}
@@ -88,9 +192,11 @@ function App() {
             ) : (
                 <Quiz
                     quiz={quiz}
+>>>>>>> 78134b87f04c9ddbde94371909595347d648bcde
                     onClickButton={onClickButton}
-                    counter={counter}
                 />
+            ) : (
+                <div>Doğru cevap sayısı: {score}</div>
             )}
         </div>
     );
