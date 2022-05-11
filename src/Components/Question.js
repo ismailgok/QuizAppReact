d4262f13e6679661fa17eaf11df81548b7d30bf6
İsmail Gök <ismayilgok16@gmail.com>
2022-05-11 14:35:40 +0300

    Update Question.js

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,7 +1,7 @@
 const Question = ({ questions, currentQuestion, onClickButton }) => {
     return (
         <div className="question">
-            <span>Soru:{currentQuestion + 1}</span>{" "}
+            <span>Soru:{currentQuestion + 1}</span>
             <h2>{questions[currentQuestion].title}</h2>
             {questions[currentQuestion].answers.map((item, index) => (
                 <button key={index} onClick={() => onClickButton(item.correct,item.answer)}>
